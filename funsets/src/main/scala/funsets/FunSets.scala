5699fea9c476b3d8c372fbeb1f0f56d3b36dfcdb
Junyuan Xue <junyuanxue.is@gmail.com>
2017-05-07 22:07:17 +0100

    refactor

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -26,19 +26,19 @@ object FunSets {
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-    def union(s: Set, t: Set): Set = x => contains(s, x) || contains(t, x)
+    def union(s: Set, t: Set): Set = x => s(x) || t(x)
   
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-    def intersect(s: Set, t: Set): Set = x => contains(s, x) && contains(t, x)
+    def intersect(s: Set, t: Set): Set = x => s(x) && t(x)
   
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-    def diff(s: Set, t: Set): Set = x => contains(s, x) && !contains(t, x)
+    def diff(s: Set, t: Set): Set = x => s(x) && !t(x)
   
   /**
    * Returns the subset of `s` for which `p` holds.
